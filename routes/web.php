a31fbfa3added58e0e55d7412fa830751ac99316
Jonatas Gama Souza <gamasouza.jonatas@gmail.com>
2023-09-19 22:29:49 -0300

    configuração de email para reset de senha

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TarefaController;
+use App\Mail\MensagemTesteEmail;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('tarefa', TarefaController::class);
+Route::get("/mail", function(){
+    return new MensagemTesteEmail;
+});
